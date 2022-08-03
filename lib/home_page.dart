a4599146266b24a3657f9f8af7df64e25a72c4e4
Danvick Miller <danvickmiller@live.com>
2022-08-03 23:47:06 +0300

    feat: added go_router for navigation

---
+++ lib/home_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  static const routeName = 'home';
+
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container());
+  }
+}
