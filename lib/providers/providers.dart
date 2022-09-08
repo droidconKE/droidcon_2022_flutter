8274f068425a357dc9a5f33162cf735508a587e0
Samuel Baraka <sababuvercetti@gmail.com>
2022-09-08 11:47:32 +0300

    feat: Created system and dark mode providers to handle the app brightness

---
+++ lib/providers/providers.dart
@@ -0,0 +1,2 @@
+export 'theme/system_theme_provider.dart';
+export 'theme/dark_theme_provider/dark_theme_provider.dart';
---
+++ lib/providers/theme/system_theme_provider.dart
@@ -0,0 +1,21 @@
+import 'package:hydrated_riverpod/hydrated_riverpod.dart';
+
+final systemThemeProvider = StateNotifierProvider<SystemThemeProvider, bool>(
+    (_) => SystemThemeProvider(false));
+
+class SystemThemeProvider extends HydratedStateNotifier<bool> {
+  SystemThemeProvider(super.state);
+  toggle() {
+    state = !state;
+  }
+
+  @override
+  bool? fromJson(Map<String, dynamic> json) {
+    return json['value'];
+  }
+
+  @override
+  Map<String, dynamic>? toJson(bool state) {
+    return {'value': state};
+  }
+}
