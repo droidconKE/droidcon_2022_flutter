a8b262674401d025c6788e976a86d468c2584d26
Samuel Baraka <sababuvercetti@gmail.com>
2022-09-08 11:48:09 +0300

    feat: Implemented the theme selection at the app root

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,24 @@
+import 'package:droidcon_app/providers/providers.dart';
 import 'package:droidcon_app/router/router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class DroidconApp extends StatelessWidget {
+class DroidconApp extends ConsumerWidget {
   const DroidconApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp.router(
       title: 'DroidconKe 2022',
+      theme: ThemeData(),
+      darkTheme: ThemeData(),
+
+      /// This adjusts the theme dynamically  based on the users preferences
+      themeMode: ref.watch(systemThemeProvider)
+          ? ThemeMode.system
+          : ref.watch(darkThemeProvider)
+              ? ThemeMode.dark
+              : ThemeMode.light,
       routeInformationParser: router.routeInformationParser,
       routerDelegate: router.routerDelegate,
       routeInformationProvider: router.routeInformationProvider,
