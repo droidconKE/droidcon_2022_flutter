b27626d8a934354baacbeb4315aff124f5d8a366
Samuel Baraka <sababuvercetti@gmail.com>
2022-09-12 12:35:24 +0300

    chore: Added color file to have all colors

---
+++ lib/user_interfaces/colors/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color lightGrayBackgroundColor = Color(0xffF5F5F5);
+  static const Color blueDroidconColor = Color(0xff000CEB);
+  static const Color orangeDroidconColor = Color(0xffFF6E4D);
+  static const Color greyDarkThemeBackground = Color(0xff20201E);
+}
