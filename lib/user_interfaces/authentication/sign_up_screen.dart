22acc690bd91ee1561202e946d55b7a7a4660e86
Samuel Baraka <sababuvercetti@gmail.com>
2022-09-12 12:36:11 +0300

    feat: Base signup page screen implementation

--- lib/user_interfaces/authentication/sign_up_screen.dart
+++ lib/user_interfaces/authentication/sign_up_screen.dart
@@ -1,5 +1,8 @@
+import 'package:droidcon_app/user_interfaces/authentication/widgets/google_button.dart';
 import 'package:droidcon_app/user_interfaces/authentication/widgets/signup_image_background.dart';
+import 'package:droidcon_app/user_interfaces/colors/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SignUpScreen extends StatelessWidget {
   static String routeName = 'signup';
@@ -8,8 +11,42 @@ class SignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: const [SignUpSVGBackground()],
-    ));
+        backgroundColor: Theme.of(context).brightness == Brightness.dark
+            ? AppColors.greyDarkThemeBackground
+            : Colors.white,
+        body: Stack(
+          children: [
+            //The Background image
+            const Positioned(
+                left: 0, right: 0, top: 0, child: SignUpSVGBackground()),
+            Positioned(
+                top: 25,
+                left: 25,
+                child: IconButton(
+                    onPressed: () {
+                      GoRouter.of(context).pop();
+                    },
+                    icon: const Icon(
+                      Icons.arrow_back,
+                      color: Colors.white,
+                    ))),
+            Positioned(
+                top: 180,
+                left: 0,
+                right: 0,
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    GoogleButton(
+                      onTap: () {
+                        print('object');
+                      },
+                    )
+                  ],
+                ))
+          ],
+        ));
   }
 }
