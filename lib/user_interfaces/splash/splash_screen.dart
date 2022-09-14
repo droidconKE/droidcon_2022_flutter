5ee5251568121562a1ae444ebb78d09a1a9cc402
Samuel Baraka <sababuvercetti@gmail.com>
2022-09-14 13:05:33 +0300

    chore: fixed lint

--- lib/user_interfaces/splash/splash_screen.dart
+++ lib/user_interfaces/splash/splash_screen.dart
@@ -1,3 +1,4 @@
+import 'package:droidcon_app/user_interfaces/widgets/theme_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -14,8 +15,15 @@ class _SplashScreenState extends ConsumerState<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 2)).then((value) {
-      GoRouter.of(context).go('/signup');
+
+    Future.delayed(const Duration(seconds: 2)).then((value) async {
+      await showDialog(
+          context: context,
+          builder: (context) {
+            return const ThemeDialog();
+          });
+      // ignore: use_build_context_synchronously
+      GoRouter.of(context).push('/signup');
     });
   }
 
