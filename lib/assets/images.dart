148ce8f5fd84dfb43edb2ce5bd877e601afb8857
ndungudedan <dnkibere@gmail.com>
2022-10-08 18:00:01 +0300

    chore: organized asset images into a single file

---
+++ lib/assets/images.dart
@@ -0,0 +1,19 @@
+class AssetImages {
+  static const earlyCampLogo = 'assets/images/early_camp_logo.png';
+  static const hoverLogo = 'assets/images/hover_logo.png';
+  static const jetbrainsLogo = 'assets/images/jetbrains_logo.png';
+  static const kotlinLogo = 'assets/images/kotlin_logo.png';
+  static const googleLogo = 'assets/images/google.png';
+  static const kotlinKeLogo = 'assets/images/kotlin_ke_logo.png';
+  static const tiskosLogo = 'assets/images/tiskos_logo.png';
+  static const droidconLogo = 'assets/images/droidcon_logo.png';
+  static const andelaLogo = 'assets/images/andela_logo.png';
+  static const appsLabLogo = 'assets/images/apps_lab_logo.png';
+  static const android254Logo = 'assets/images/android_254_logo.png';
+  static const droidconBanner = 'assets/images/droidcon_banner.png';
+  static const cfsBanner = 'assets/images/cfs_banner.png';
+  static const cfsRibbon = 'assets/images/cfs_ribbon.png';
+  static const aboutIcon = 'assets/images/about_icon.png';
+  static const lockIcon = 'assets/images/lock_icon.png';
+  static const playIcon = 'assets/images/play_icon.png';
+}
