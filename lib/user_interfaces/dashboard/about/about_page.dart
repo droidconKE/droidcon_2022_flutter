7ebae7d4746364211df27b6e6850aa5d7b9205a5
Samuel Baraka <sababuvercetti@gmail.com>
2022-10-17 16:25:06 +0300

    feat: Added home page widget files

---
+++ lib/user_interfaces/dashboard/about/about_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class AboutPage extends StatefulWidget {
+  const AboutPage({super.key});
+
+  @override
+  State<AboutPage> createState() => _AboutPageState();
+}
+
+class _AboutPageState extends State<AboutPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
