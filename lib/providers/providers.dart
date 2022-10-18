c110d958a6a5e2c173cd21bb6174a8ccd95dd820
Samuel Baraka <sababuvercetti@gmail.com>
2022-10-18 11:26:33 +0300

    chore: Exported more providers to the main provider export file

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,3 +1,6 @@
 export 'theme/system_theme_provider.dart';
 export 'theme/dark_theme_provider/dark_theme_provider.dart';
 export 'dashboard/home_provider.dart';
+export 'bottom_navigation/bottom_navigation_provider.dart';
+export 'dashboard/home_provider.dart';
+export 'token_provider/token_provider.dart';
