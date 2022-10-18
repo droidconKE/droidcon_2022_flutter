01575a77373bee0b9b03f48e6b333fbc052a66db
Samuel Baraka <sababuvercetti@gmail.com>
2022-10-18 11:25:51 +0300

    feat: Added the token provider to store the token in a hydrated storage

---
+++ lib/providers/token_provider/token_provider.dart
@@ -0,0 +1,26 @@
+import 'package:hydrated_riverpod/hydrated_riverpod.dart';
+
+class TokenProvider extends HydratedStateNotifier<String> {
+  TokenProvider() : super('');
+
+  /// Set the token from here
+  /// Called after successful login
+  void setToken(String token) {
+    state = token;
+  }
+
+  /// This override reads the token value from the storage (Stored as a map where the key is 'token'
+  /// and it's value is the token value)
+  @override
+  String fromJson(Map<String, dynamic> json) {
+    return json['token'] as String;
+  }
+
+  /// This override writes the token value to the storage (Stored as a map where the key is 'token'
+  /// and it's value is the token value)
+
+  @override
+  Map<String, dynamic> toJson(String state) {
+    return {'token': state};
+  }
+}
