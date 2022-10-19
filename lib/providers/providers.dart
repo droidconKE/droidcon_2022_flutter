7e9ce2652d9baa24256a79d797336acc10a4867e
Samuel Baraka <sababuvercetti@gmail.com>
2022-10-19 11:41:30 +0300

    connected the applications to firebase

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -2,5 +2,4 @@ export 'theme/system_theme_provider.dart';
 export 'theme/dark_theme_provider/dark_theme_provider.dart';
 export 'home/home_provider.dart';
 export 'bottom_navigation/bottom_navigation_provider.dart';
-export 'dashboard/home_provider.dart';
 export 'token_provider/token_provider.dart';
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,22 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import firebase_analytics
+import firebase_core
+import firebase_crashlytics
+import package_info_plus
+import path_provider_macos
+import sqflite
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+}
