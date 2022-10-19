c0194bbc3d44baabdd6c92d2dfde2b06672cfece
Samuel Baraka <sababuvercetti@gmail.com>
2022-10-19 12:37:07 +0300

    feat: Added scopes to the google sign in for profile information

--- lib/services/authentication/authentication_service.dart
+++ lib/services/authentication/authentication_service.dart
@@ -1,16 +1,25 @@
+import 'package:droidcon_app/models/models.dart';
 import 'package:droidcon_app/services/api_service/api_service.dart';
-import 'package:droidcon_app/utils/utils.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthenticationService {
-  static loginWithGoogle() async {
+  static Future<LoginResponse> loginWithGoogle() async {
     // First authenticate the user with google auth
-    GoogleSignIn googleSignIn = GoogleSignIn();
+    GoogleSignIn googleSignIn = GoogleSignIn(
+      scopes: [
+        'https://www.googleapis.com/auth/userinfo.profile',
+      ],
+    );
     final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
     GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
     // Then do the API call
+    try{
     var response = await ApiService.postData(
         path: 'social_login/google',
         data: {'access_token': googleAuth?.accessToken});
+        return LoginResponse.fromJson(response);
+    }on String catch(e){
+      throw e;
+    }
   }
 }
