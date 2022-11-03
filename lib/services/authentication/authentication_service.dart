fe72f1c6f16746c8e6874ca71c05b3b2088dc0ee
Samuel Baraka <sababuvercetti@gmail.com>
2022-11-03 12:28:13 +0300

    fix: Removed unused scopes

--- lib/services/authentication/authentication_service.dart
+++ lib/services/authentication/authentication_service.dart
@@ -6,20 +6,16 @@ class AuthenticationService {
   /// Login with Google
   static Future<LoginResponse> loginWithGoogle() async {
     // First authenticate the user with google auth
-    GoogleSignIn googleSignIn = GoogleSignIn(
-      scopes: [
-        'https://www.googleapis.com/auth/userinfo.profile',
-      ],
-    );
+    GoogleSignIn googleSignIn = GoogleSignIn();
     final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
     GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
     // Then do the API call
-    try{
-    var response = await ApiService.postData(
-        path: 'social_login/google',
-        data: {'access_token': googleAuth?.accessToken});
-        return LoginResponse.fromJson(response);
-    }on String {
+    try {
+      var response = await ApiService.postData(
+          path: 'social_login/google',
+          data: {'access_token': googleAuth?.accessToken});
+      return LoginResponse.fromJson(response);
+    } on String {
       rethrow;
     }
   }
