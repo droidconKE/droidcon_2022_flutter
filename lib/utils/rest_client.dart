04b4dc7f4121462b18da9fb6ee6b118082cee681
Danvick Miller <danvickmiller@live.com>
2022-11-12 13:05:09 +0300

    chore: report dio performance to firebase

--- lib/utils/rest_client.dart
+++ lib/utils/rest_client.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:droidcon_app/utils/utils.dart';
+import 'package:firebase_performance_dio/firebase_performance_dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
@@ -75,6 +76,7 @@ class RestClient {
           compact: true,
           maxWidth: 90,
         ),
+      DioFirebasePerformanceInterceptor(),
     ]);
 
     _dioNoAUth!.interceptors.addAll([
